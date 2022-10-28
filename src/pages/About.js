825df0281941e2ccfe7a590560047040eb0b55e7
delioncourts <delioncourtles@yandex.ru>
2022-10-28 21:25:12 +0300

    Added p in About, dist - test for PR

--- src/pages/About.js
+++ src/pages/About.js
@@ -4,6 +4,7 @@ export function About() {
   return (
     <>
       <h1 data-testid="page-title">About</h1>
+      <p>Have a wonderful day!</p>
       <p>This is an example application.</p>
     </>
   );
